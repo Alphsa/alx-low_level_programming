97e56276d9ef2e1eca92c47caebd3e3923a75d9c
Alphsa <sandrafankou@gmail.com>
2022-09-12 07:36:30 -0700

    Print alphabets lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch, sh;
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+for (sh = 'A'; sh <= 'Z'; ch++)
+{
+putchar(sh);
+}
+return (0);
+}
