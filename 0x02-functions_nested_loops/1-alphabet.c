6fd40b55fea84bc68e8857373540f9593006c682
Alphsa <sandrafankou@gmail.com>
2022-09-13 18:05:17 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "_putchar"
+/**
+* print_alphabet - prints alphabet
+*/
+void print_alphabet(void)
+{
+char s;
+for (s = 'a'; s <= 'z'; s++)
+_putchar(s);
+_putchar('\n');
+}
