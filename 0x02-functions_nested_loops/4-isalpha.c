457f6eedb106d8a3e087f57a959e0920468a3caf
Alphsa <sandrafankou@gmail.com>
2022-09-13 19:10:38 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,9 +5,10 @@
 * @c: The character in ASCII code
 * Return: 1 for letters. Returns 0 otherwise
 */
-
 int _isalpha(int c)
+
 {
+
 if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 {
 return (1);
