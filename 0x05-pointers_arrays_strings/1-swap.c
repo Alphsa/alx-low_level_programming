540f5f37fd1430d5d788f27e283c867d6ade21e6
Alphsa <sandrafankou@gmail.com>
2022-09-19 15:18:46 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap two integers
+* Return: void
+*/
+void swap_int(int *a, int *b)
+{
+int a;
+int b;
+int c;
+int d;
+c=a;
+d=b
+a=d;
+b=c;
+}
+ 
+ 
