84aaab4f732dfd35035810aff52ecb57c074a927
Alphsa <sandrafankou@gmail.com>
2022-11-03 06:12:46 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,7 @@
  * main - program that copies the content of a file to another file
  * @argc: num argument
  * @argv: string argument
- * eturn: 0
+ * Return: 0
 */
 
 int main(int argc, char *argv[])
