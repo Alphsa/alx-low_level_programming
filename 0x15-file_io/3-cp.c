3732a97abc429f94b8b269b9fd56ad7f2abb6ceb
Alphsa <sandrafankou@gmail.com>
2022-11-03 06:20:12 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,6 +23,7 @@ if (file_from == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
+}
 file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (file_to == -1)
